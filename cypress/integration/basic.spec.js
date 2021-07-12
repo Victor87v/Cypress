41fbc83a67c14ad8594998836dd72aa66604f359
Victor87v <victorvalente456@gmail.com>
2021-07-12 17:59:55 -0300

    Segunda-Feira

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -4,15 +4,36 @@ describe('Cyoress Básico',() => {
     it.only('Visitar uma página e fazer uma assertiva no título', () =>{
         cy.visit('http://wcaquino.me/cypress/componentes.html')
 
-        cy.pause()
         // const title = cy.title()
         // console.log(title)
         cy.title().should('be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
 
+        cy.title().should(title => {
+            console.log(title)
+        })
+
         cy.title()
         .should('be.equal', 'Campo de Treinamento')
         .and('contain', 'Campo')
+
+        let syncTitle
+
+        cy.title().then(title => {
+            console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle = title
+        })
+
+        cy.get('[data-cy=dataSobrenome]').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
     })
 
     it('Procurar interagir com um elemento', ()=>{
@@ -21,6 +42,7 @@ describe('Cyoress Básico',() => {
         cy.get('#buttonSimple')
         .click()
         .should('have.value', 'Obrigado!')
-
+    
     })
+    
 })
---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,74 @@
+/// <reference types="cypress" />
+
+describe('Esperas...', () => {
+    
+    before(() => {
+        cy.visit('http://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+    it('Deve aguardar elemento estar disponivel',()=> {
+        
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+
+        cy.get('#novoCampo').type('funciona')
+
+    })
+
+    it.only('Deve aguardar elemento estar disponivel',()=> {
+        
+        
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+        // .should('not.exist')
+        .should('exist')
+        .type('Funciona')
+        
+    })
+
+    it.only('Uso do find',() => {
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain','Item 1')
+
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
+
+    it.only('Uso do timeout', () => {
+        // cy.get('#buttonDelay').click()
+        // cy.get('#novoCampo').should('exist')
+
+        //cy.get('#buttonListDOM').click()
+        // cy.wait(5000)
+        //cy.get('#lista li span')
+       //     .should('contain', 'Item 2')
+
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span', {timeout: 30000})
+            .should('have.length', 1)
+        cy.get('#lista li span')
+            .should('have.length', 2)
+
+    })
+
+    it.only('Click retry', () => {
+        cy.get('#buttonCount')
+            .click()
+            .click()
+            .should('have.value', '111')
+    })
+
+    it.only('Should vs Then', () => {
+        cy.get('#buttonListDOM').then($el => {
+            expect($el).to.have.length(1)
+            cy.get('#buttonList')
+        })
+    })
+})
